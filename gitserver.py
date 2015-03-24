016081e057b3af67025cd021af603eca0e6d9eaa
Cheyenne T. Greatorex <ctg@eddie>
2015-03-24 06:38:21 -0400

    standardized quotation marks, added logging

--- gitserver.py
+++ gitserver.py
@@ -23,9 +23,9 @@ PERM_WRITE = 2
 # git-upload-archive
 # git-receive-pack
 COMMANDS = {
-    "git-upload-pack" : PERM_READ,
-    "git-upload-archive" : PERM_READ,
-    "git-receive-pack" : PERM_WRITE,
+    'git-upload-pack' : PERM_READ,
+    'git-upload-archive' : PERM_READ,
+    'git-receive-pack' : PERM_WRITE,
 }
 
 
@@ -54,9 +54,9 @@ CONFIG_FILES = [
 ]
 
 CONFIG_OPTS = {
-    "log_file" : "",
-    "repo_dir" : "",
-    "database" : "",
+    'log_file' : '',
+    'repo_dir' : '',
+    'database' : '',
 }
 
 
@@ -70,48 +70,70 @@ CONFIG_OPTS = {
 # repository.readable(user)
 
 
-
 # parse the configuration file
 config = ConfigParser.ConfigParser()
 config_files = config.read(CONFIG_FILES)
 if not config_files:
-    sys.stderr.write("Configuration error: file not found. Must be one of: %s\n" % ", ".join(s for s in CONFIG_FILES))
+    sys.stderr.write('Configuration error: file not found. Must be one of: %s\n' % ', '.join(s for s in CONFIG_FILES))
     sys.exit(-1)
 
 if not config.has_section('default'):
-    sys.stderr.write("Configuration error: [default] section not found.\n")
+    sys.stderr.write('Configuration error: [default] section not found.\n')
     sys.exit(-1)
 
 for opt in CONFIG_OPTS:
     if not config.has_option('default', opt):
-        sys.stderr.write("Configuration error: [default] section missing %s.\n" % opt)
+        sys.stderr.write('Configuration error: [default] section missing "%s".\n' % opt)
         sys.exit(-1)
 
     CONFIG_OPTS[opt] = config.get('default', opt)
 
     if not CONFIG_OPTS[opt]:
-        sys.stderr.write("Configuration error: option %s is empty.\n" % opt)
+        sys.stderr.write('Configuration error: option "%s" is empty.\n' % opt)
         sys.exit(-1)
 
+
+# setup logging
+logging.basicConfig(filename=CONFIG_OPTS['log_file', format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
+
+
+# parse command line arguments
+parser = argparse.ArgumentParser(description='Process a git command as called via an SSH connection.')
+parser.add_argument('-u', '--username', required=True, help='Name of the gitserver user account.')
+args = parser.parse_args()
+
+username = args.username
+
+
+logging.info('User "%s" connected to git server.' % username)
 
 
 # parse the specified command
 original_cmd = os.environ.get('SSH_ORIGINAL_COMMAND')
 
 if original_cmd == None:
-    sys.stderr.write("You have successfully authenticated, but this server does not provide shell access.\n")
+    msg = 'No command provided in SSH_ORIGINAL_COMMAND, user attempted to access shell.'
+    logging.critical(msg)
+    msg = 'You have successfully authenticated, but this server does not provide shell access.\n'
+    sys.stderr.write(msg)
     sys.exit(-1)
 
 parsed_cmd = string.split(original_cmd, ' ')
 
 if len(parsed_cmd) != 2:
-    sys.stderr.write("You must pass a single command with exactly one argument.\n")
+    msg = 'Received invalid command "%s".' % original_cmd
+    logging.critical(msg)
+    msg = 'You must pass a single command with exactly one argument.\n'
+    sys.stderr.write(msg)
     sys.exit(-1)
 
 command = parsed_cmd[0]
 repository = re.sub('\'', '', parsed_cmd[1])
 
 if not command in COMMANDS:
-    sys.stderr.write("'%s' is not a valid command. Must be one of: %s\n" % (command, ", ".join(s for s in COMMANDS.keys())))
+    msg = 'Received invalid command "%s".' % original_cmd
+    logging.critical(msg)
+    msg = '"%s" is not a valid command. Must be one of: %s\n' % (command, ', '.join(s for s in COMMANDS.keys()))
+    sys.stderr.write(msg)
     sys.exit(-1)
 
