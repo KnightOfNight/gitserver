481d7d67dd343fbc4c5dbfb89d3fca3dca7148ac
Cheyenne T. Greatorex <cgreatorex@Lightsaber.local>
2015-03-26 15:23:03 -0400

    added stug of manager script

---
+++ manager.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# load config, need repo dir
+
+# repo create <repo>
+
+# repo delete <repo>
+
+# user create <user>
+
+# user delete <user>
+
+# permission add read|write <repo> <user>
+
+# permission remove read|write <repo> <user>
+
