9dfcbf88fd59b3bd7f2b2287f7d618672c6f9d15
Cheyenne T. Greatorex <ctg@eddie>
2015-03-27 07:26:38 -0400

    additional subparser config

--- manager.py
+++ manager.py
@@ -28,14 +28,17 @@ parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(help = 'commands')
 
 parser_repo = subparsers.add_parser('repo', help = 'repository management')
+parser_repo.set_defaults(mode='repo')
 parser_repo.add_argument('command', choices = [ 'create', 'delete'], help = 'action to take')
 parser_repo.add_argument('name', help = 'name of the repository')
 
 parser_user = subparsers.add_parser('user', help = 'user management')
+parser_user.set_defaults(mode='user')
 parser_user.add_argument('command', choices = [ 'create', 'update', 'delete'], help = 'action to take')
 parser_user.add_argument('name', help = 'name of the user')
 
 parser_perm = subparsers.add_parser('perm', help = 'permission management')
+parser_perm.set_defaults(mode='perm')
 parser_perm.add_argument('command', choices = [ 'add', 'remove'], help = 'action to take')
 parser_perm.add_argument('user', help = 'name of the user')
 parser_perm.add_argument('permission', choices = [ Permission.name[Permission.read], Permission.name[Permission.write] ], help = 'permission to grant')
@@ -43,3 +46,4 @@ parser_perm.add_argument('repo', help = 'name of the repository')
 
 args = parser.parse_args()
 
+print args.command
