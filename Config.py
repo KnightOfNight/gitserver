6144bd948c8a4214a6310c796f247a7e52bdfe45
Cheyenne T. Greatorex <ctg@eddie>
2015-03-28 07:31:00 -0400

    moved configuration parsing to its own library

---
+++ Config.py
@@ -0,0 +1,36 @@
+
+
+import ConfigParser
+import os
+
+from GitServer import fatal_error
+
+
+def get_config:
+    config_opts = {
+        'log_file' : '',
+        'repo_dir' : '',
+        'database' : '',
+    }
+
+    config = ConfigParser.ConfigParser()
+
+    config_files = config.read(['/etc/gitserver.cfg', os.path.expanduser('~/.gitserver.cfg')])
+
+    if not config_files:
+        fatal_error('configuration file not found')
+
+    if not config.has_section('default'):
+        fatal_error('configuration error, [default] section not found')
+
+    for opt in config_opts:
+        if not config.has_option('default', opt):
+            fatal_error('configuration error, [default] section missing option "%s"' % opt)
+
+        config_opts[opt] = config.get('default', opt)
+
+        if not config_opts[opt]:
+            fatal_error('configuration error, option "%s" is empty' % opt)
+
+    return(config_opts)
+
